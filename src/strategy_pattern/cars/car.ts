2cb788e678b8587f6049d6f553f798310263015d
dhanasaitholeti <dhanasai5566@gmail.com>
2025-02-13 16:59:16 +0530

    added the refuel example

--- src/strategy_pattern/cars/car.ts
+++ src/strategy_pattern/cars/car.ts
@@ -1,14 +1,25 @@
+import { FuelStrategy } from "./fuel";
 import { TransmissionStrategy } from "./transmission";
 
 export class Car {
   private transmission: TransmissionStrategy;
+  private fuel: FuelStrategy;
 
-  constructor(transmission: TransmissionStrategy) {
+  constructor(transmission: TransmissionStrategy, fuel: FuelStrategy) {
     this.transmission = transmission;
+    this.fuel = fuel;
   }
 
   setTransmission(transmission: TransmissionStrategy): void {
     this.transmission = transmission;
+  }
+
+  setFuel(fuel: FuelStrategy): void {
+    this.fuel = fuel;
+  }
+
+  refuel(): void {
+    this.fuel.refuel();
   }
 
   changeGear(): void {
--- src/strategy_pattern/index.ts
+++ src/strategy_pattern/index.ts
@@ -1,18 +1,22 @@
 import { Car } from "./cars/car";
+import { Diesel, Electric, Gasoline } from "./cars/fuel";
 import {
   AutomaticTransmission,
   ManualTransmission,
   SemiAutomaticTransmission,
 } from "./cars/transmission";
 
-const manualCar = new Car(new ManualTransmission());
-manualCar.changeGear(); 
+const manualCar = new Car(new ManualTransmission(), new Gasoline());
+manualCar.changeGear();
 
-const automaticCar = new Car(new AutomaticTransmission());
-automaticCar.changeGear(); 
+const automaticCar = new Car(new AutomaticTransmission(), new Electric());
+automaticCar.changeGear();
 
-const semiAutomaticCar = new Car(new SemiAutomaticTransmission());
-semiAutomaticCar.changeGear(); 
+const semiAutomaticCar = new Car(new SemiAutomaticTransmission(), new Diesel());
+semiAutomaticCar.changeGear();
 
 manualCar.setTransmission(new AutomaticTransmission());
-manualCar.changeGear(); 
+semiAutomaticCar.setFuel(new Electric());
+
+manualCar.changeGear();
+semiAutomaticCar.refuel();
